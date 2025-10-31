230b7be31618100d8252d1fa20d7708115f66e44
sarojinimanikandan777-del <sarojinimanikandan777@gmail.com>
2025-10-31 19:16:05 +0530

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new mongoose.Schema({
+name: { type: String, required: true },
+email: { type: String, required: true, unique: true },
+passwordHash: { type: String, required: true },
+role: { type: String, enum: ['user', 'admin'], default: 'user' },
+createdAt: { type: Date, default: Date.now }
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
